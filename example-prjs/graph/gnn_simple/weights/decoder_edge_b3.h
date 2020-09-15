3bbcb8cf84dbfedfee765af710ea4d763adb766a
vesal-rm <vrazavim@prp-gpu-1.t2.ucsd.edu>
2020-09-14 17:39:39 -0700

    realistic weights

--- example-prjs/graph/gnn_simple/weights/decoder_edge_b3.h
+++ example-prjs/graph/gnn_simple/weights/decoder_edge_b3.h
@@ -1,7 +1,7 @@
 //Numpy array shape [1]
-//Min 0.000000000000
-//Max 0.000000000000
-//Number of zeros 1
+//Min 0.079212802501
+//Max 0.079212802501
+//Number of zeros 0
 
 #ifndef DECODER_EDGE_B3_H_
 #define DECODER_EDGE_B3_H_
@@ -9,7 +9,7 @@
 #ifndef __SYNTHESIS__
 ap_fixed<16,6> decoder_edge_b3[1];
 #else
-ap_fixed<16,6> decoder_edge_b3[1] = {0.000000};
+ap_fixed<16,6> decoder_edge_b3[1] = {0.079213};
 #endif
 
 #endif
