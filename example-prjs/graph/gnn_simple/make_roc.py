b6fac175bca4059bfef4b47b5ce3d5dd1f9d339e
Javier M. Duarte <jduarte@ucsd.edu>
2020-10-05 00:47:51 -0700

    update

--- example-prjs/graph/gnn_simple/make_roc.py
+++ example-prjs/graph/gnn_simple/make_roc.py
@@ -20,11 +20,12 @@ res = []
 prd = []
 tgt = []
 
-runCsim = False
+runCsim = True
+bit = 16
 
-os.makedirs('./tb_data', exist_ok=True)
-for i in range(100): #90623
-    if runCsim:
+os.makedirs('./tb_data_%i'%bit, exist_ok=True)
+for i in range(10): #90623
+    if runCsim and not os.path.isfile('./tb_data_%i/tb_output_edge_predictions_%05d.dat'%(bit,i)):
         copyfile('/scratch/data/vrazavim/exatrkx-neurips19/gnn-tracking/tb_input_edge_features_%05d.dat'%i, './tb_input_edge_features.dat')
         copyfile('/scratch/data/vrazavim/exatrkx-neurips19/gnn-tracking/tb_input_node_features_%05d.dat'%i, './tb_input_node_features.dat')
         copyfile('/scratch/data/vrazavim/exatrkx-neurips19/gnn-tracking/tb_output_edge_predictions_%05d.dat'%i, './tb_output_edge_predictions.dat')
@@ -32,22 +33,22 @@ for i in range(100): #90623
         copyfile('/scratch/data/vrazavim/exatrkx-neurips19/gnn-tracking/tb_receivers_%05d.dat'%i, './tb_receivers.dat')
         copyfile('/scratch/data/vrazavim/exatrkx-neurips19/gnn-tracking/tb_senders_%05d.dat'%i, './tb_senders.dat')
         os.system('source /xilinx/Vivado/2019.2/settings64.sh; vivado_hls -f build_prj.tcl')
-        copyfile('./myproject_prj/solution_roc/csim/build/tb_output_edge_labels.dat', './tb_data/tb_output_edge_labels_%05d.dat'%i)
-        copyfile('./tb_output_edge_predictions.dat', './tb_output_edge_predictions_%05d.dat'%i)
-        copyfile('./tb_output_edge_targets.dat', './tb_output_edge_targets_%05d.dat'%i)
+        copyfile('./myproject_prj/solution_roc/csim/build/tb_output_edge_labels.dat', './tb_data_%i/tb_output_edge_labels_%05d.dat'%(bit,i))
+        copyfile('./tb_output_edge_predictions.dat', './tb_data_%i/tb_output_edge_predictions_%05d.dat'%(bit,i))
+        copyfile('./tb_output_edge_targets.dat', './tb_data_%i/tb_output_edge_targets_%05d.dat'%(bit,i))
             
-    with open('./tb_output_edge_predictions_%05d.dat'%i, 'r') as prd_file:
+    with open('./tb_data_%i/tb_output_edge_predictions_%05d.dat'%(bit,i), 'r') as prd_file:
         for line in prd_file.readlines():
             prd_list = [float(x) for j, x in enumerate(line.split(' ')) if (j < 57) if x.strip()]
         n_edges = len(prd_list)
         prd.extend(prd_list)
 
-    with open('./tb_output_edge_targets_%05d.dat'%i, 'r') as tgt_file:
+    with open('./tb_data_%i/tb_output_edge_targets_%05d.dat'%(bit,i), 'r') as tgt_file:
         for line in tgt_file.readlines():
             tgt_list = [int(float(x)) for j, x in enumerate(line.split(' ')) if (j < 57) if x.strip()]
         tgt.extend(tgt_list)
 
-    with open('./tb_data/tb_output_edge_labels_%05d.dat'%i, 'r') as res_file:
+    with open('./tb_data_%i/tb_output_edge_labels_%05d.dat'%(bit,i), 'r') as res_file:
         for line in res_file.readlines():
             res_list = [float(x) for j, x in enumerate(line.split(' ')) if (j < n_edges) if x.strip()]
         res.extend(res_list)
@@ -101,7 +102,7 @@ plt.legend(loc="lower right")
 plt.savefig('python_roc.pdf')
 
 plt.figure()
-plt.scatter(prdctn,result,marker='.')
+plt.scatter(prdctn,result,marker='o',alpha=0.5)
 plt.xlabel('python')
 plt.ylabel('csim')
 plt.savefig('scatter.pdf')
