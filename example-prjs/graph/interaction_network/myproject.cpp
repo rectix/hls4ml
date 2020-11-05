aba389b466dc2caff176a7254dd67333bff77d31
vesal-rm <vrazavim@prp-gpu-1.t2.ucsd.edu>
2020-11-05 00:50:42 -0800

    IN work in progress

--- example-prjs/graph/interaction_network/myproject.cpp
+++ example-prjs/graph/interaction_network/myproject.cpp
@@ -76,86 +76,59 @@ void myproject(
   static bool loaded_weights = false;
  if (!loaded_weights) {
    //hls-fpga-machine-learning insert load weights                                                                           
-   nnet::load_weights_from_txt<model_default_t, 12>(encoder_node_w0, "encoder_node_w0.txt");
-   nnet::load_weights_from_txt<model_default_t, 4>(encoder_node_b0, "encoder_node_b0.txt");
-   nnet::load_weights_from_txt<model_default_t, 16>(encoder_node_w1, "encoder_node_w1.txt");
-   nnet::load_weights_from_txt<model_default_t, 4>(encoder_node_b1, "encoder_node_b1.txt");
-   nnet::load_weights_from_txt<model_default_t, 16>(encoder_edge_w0, "encoder_edge_w0.txt");
-   nnet::load_weights_from_txt<model_default_t, 4>(encoder_edge_b0, "encoder_edge_b0.txt");
-   nnet::load_weights_from_txt<model_default_t, 16>(encoder_edge_w1, "encoder_edge_w1.txt");
-   nnet::load_weights_from_txt<model_default_t, 4>(encoder_edge_b1, "encoder_edge_b1.txt");
-   nnet::load_weights_from_txt<model_default_t, 48>(core_edge_w0, "core_edge_w0.txt");
-   nnet::load_weights_from_txt<model_default_t, 4>(core_edge_b0, "core_edge_b0.txt");
-   nnet::load_weights_from_txt<model_default_t, 16>(core_edge_w1, "core_edge_w1.txt");
-   nnet::load_weights_from_txt<model_default_t, 4>(core_edge_b1, "core_edge_b1.txt");
-   nnet::load_weights_from_txt<model_default_t, 32>(core_node_w0, "core_node_w0.txt");
-   nnet::load_weights_from_txt<model_default_t, 4>(core_node_b0, "core_node_b0.txt");
-   nnet::load_weights_from_txt<model_default_t, 16>(core_node_w1, "core_node_w1.txt");
-   nnet::load_weights_from_txt<model_default_t, 4>(core_node_b1, "core_node_b1.txt");
-   nnet::load_weights_from_txt<model_default_t, 16>(decoder_edge_w0, "decoder_edge_w0.txt");
-   nnet::load_weights_from_txt<model_default_t, 4>(decoder_edge_b0, "decoder_edge_b0.txt");
-   nnet::load_weights_from_txt<model_default_t, 16>(decoder_edge_w1, "decoder_edge_w1.txt");
-   nnet::load_weights_from_txt<model_default_t, 4>(decoder_edge_b1, "decoder_edge_b1.txt");
-   nnet::load_weights_from_txt<model_default_t, 16>(decoder_edge_w2, "decoder_edge_w2.txt");
-   nnet::load_weights_from_txt<model_default_t, 4>(decoder_edge_b2, "decoder_edge_b2.txt");
-   nnet::load_weights_from_txt<model_default_t, 4>(decoder_edge_w3, "decoder_edge_w3.txt");
+   nnet::load_weights_from_txt<model_default_t, N_FEATURES*latent_dim>(encoder_node_w0, "encoder_node_w0.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim>(encoder_node_b0, "encoder_node_b0.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim*latent_dim>(encoder_node_w1, "encoder_node_w1.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim>(encoder_node_b1, "encoder_node_b1.txt");
+   nnet::load_weights_from_txt<model_default_t, E_FEATURES*latent_dim>(encoder_edge_w0, "encoder_edge_w0.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim>(encoder_edge_b0, "encoder_edge_b0.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim*latent_dim>(encoder_edge_w1, "encoder_edge_w1.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim>(encoder_edge_b1, "encoder_edge_b1.txt");
+   nnet::load_weights_from_txt<model_default_t, 3*latent_dim*latent_dim>(core_edge_w0, "core_edge_w0.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim>(core_edge_b0, "core_edge_b0.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim*latent_dim>(core_edge_w1, "core_edge_w1.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim>(core_edge_b1, "core_edge_b1.txt");
+   nnet::load_weights_from_txt<model_default_t, 2*latent_dim*latent_dim>(core_node_w0, "core_node_w0.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim>(core_node_b0, "core_node_b0.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim*latent_dim>(core_node_w1, "core_node_w1.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim>(core_node_b1, "core_node_b1.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim*latent_dim>(decoder_edge_w0, "decoder_edge_w0.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim>(decoder_edge_b0, "decoder_edge_b0.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim*latent_dim>(decoder_edge_w1, "decoder_edge_w1.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim>(decoder_edge_b1, "decoder_edge_b1.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim*latent_dim>(decoder_edge_w2, "decoder_edge_w2.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim>(decoder_edge_b2, "decoder_edge_b2.txt");
+   nnet::load_weights_from_txt<model_default_t, latent_dim>(decoder_edge_w3, "decoder_edge_w3.txt");
    nnet::load_weights_from_txt<model_default_t, 1>(decoder_edge_b3, "decoder_edge_b3.txt");
 
    loaded_weights = true;
  }
 #endif
   
-  //encode nodes features
-  input_t Rn[N_NODES_MAX][latent_dim];
-  #pragma HLS ARRAY_PARTITION variable=Rn complete dim=0
-  nnet::graph_independent<input_t, input_t, graph_config1>(N, Rn, encoder_node_w0, encoder_node_b0, encoder_node_w1, encoder_node_b1);
-  /*
-  for(int i = 0; i < N_NODES; i++){
-    std::cout << "Rn[" << i << "] = [";
-    for(int j = 0; j < latent_dim; j++){
-      std::cout << Rn[i][j] << ", ";
-    }
-    std::cout << "]" << std::endl;
-  }
-  */
-  //encode edge features
-  input_t Re[N_EDGES_MAX][latent_dim];
-  #pragma HLS ARRAY_PARTITION variable=Re complete dim=0
-  nnet::graph_independent<input_t, input_t, graph_config2>(E, Re, encoder_edge_w0, encoder_edge_b0, encoder_edge_w1, encoder_edge_b1);
-  /*
-  for(int i = 0; i < N_EDGES; i++){
-    std::cout << "Re[" << i << "] = [";
-    for(int j = 0; j < latent_dim; j++){
-      std::cout << Re[i][j] << ", ";
-    }
-    std::cout << "]" << std::endl;
-  }
-  */
-  //core networks
+  //interaction network
   input_t L[N_EDGES_MAX][latent_dim];
   input_t Q[N_NODES_MAX][latent_dim];
   input_t P[N_NODES_MAX][latent_dim];
   #pragma HLS ARRAY_PARTITION variable=L complete dim=0
   #pragma HLS ARRAY_PARTITION variable=Q complete dim=0
   #pragma HLS ARRAY_PARTITION variable=P complete dim=0
+
+  input_t e_logits[N_EDGES_MAX][1];
+  input_t q[N_NODES_MAX][latent_dim];
+  #pragma HLS ARRAY_PARTITION variable=e_logits complete dim=0
+  #pragma HLS ARRAY_PARTITION variable=q complete dim=0
   
   for(int i = 0; i < N_ITERS; i++){
 
-    //core edge updates
-    nnet::IN_edge_module<input_t, index_t, input_t, graph_config3>(Re, Rn, receivers, senders, L, Q, core_edge_w0, core_edge_b0, core_edge_w1, core_edge_b1);
-    //core node updates
-    nnet::IN_node_module<input_t, input_t, graph_config4>(Rn, Q, P, core_node_w0, core_node_b0, core_node_w1, core_node_b1);
+    //edge block
+    nnet::IN_edge_module<input_t, index_t, input_t, graph_config3>(E, N, receivers, senders, L, Q, core_edge_w0, core_edge_b0, core_edge_w1, core_edge_b1);
+    //node block
+    nnet::IN_node_module<input_t, input_t, graph_config4>(N, Q, P, core_node_w0, core_node_b0, core_node_w1, core_node_b1);
+    //edge block
+    nnet::IN_edge_module<input_t, index_t, input_t, graph_config3>(L, P, receivers, senders, e_logits, q, core_edge_w2, core_edge_b2, core_edge_w3, core_edge_b3);
 
   }
 
-  //decode edge features
-  input_t Ro[N_EDGES_MAX][latent_dim];
-  #pragma HLS ARRAY_PARTITION variable=Ro complete dim=0
-  nnet::graph_independent<input_t, input_t, graph_config5>(L, Ro, decoder_edge_w0, decoder_edge_b0, decoder_edge_w1, decoder_edge_b1);
-
-  input_t e_logits[N_EDGES_MAX][1];
-  #pragma HLS ARRAY_PARTITION variable=e_logits complete dim=0
-  nnet::graph_independent<input_t, input_t, graph_config6>(Ro, e_logits, decoder_edge_w2, decoder_edge_b2, decoder_edge_w3, decoder_edge_b3);
-
+  //activation function
   nnet::sigmoid_batch<input_t, input_t, sigmoid_config1>(e_logits, e);
 }
