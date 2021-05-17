c9afe784854ebf6d79e76fa9a9225da21ce24d25
vesal-rm <vesal.rm@gmail.com>
2021-05-17 15:51:35 -0700

    update arg

--- example-prjs/graph/gnn_simple/myproject.h
+++ example-prjs/graph/gnn_simple/myproject.h
@@ -10,8 +10,7 @@
 void myproject(
 	       input_t     N[N_NODES_MAX][N_FEATURES],
 	       input_t     E[N_EDGES_MAX][E_FEATURES],
-               index_t     receivers[N_EDGES_MAX][1],
-               index_t     senders[N_EDGES_MAX][1],
+               index_t     edge_index[N_EDGES_MAX][2],
 	       result_t    e[N_EDGES_MAX][1],
 	       unsigned short &const_size_in,
 	       unsigned short &const_size_out);
